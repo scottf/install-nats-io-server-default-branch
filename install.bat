7550f89d1204680e756117eefa159371ecee36fe
Scott Fauerbach <scottfauerbach@gmail.com>
2021-04-29 18:30:34 -0400

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,8 @@
+cd $GITHUB_WORKSPACE
+git clone https://github.com/nats-io/nats-server.git
+cd nats-server
+go build main.go
+copy /Y main.exe C:\Users\runneradmin\AppData\Local\Microsoft\WindowsApps\nats-server.exe
+cd ..
+rd /S /Q nats-server
+nats-server -v
